991764cfc9943c5bfc3c9521dfe67b718017f41b
Raymond Guiller N. Lam Ko <rgnlamko@headoffice.corp.bpi.com.ph>
2025-12-04 16:14:46 +0800

    add seatwork2

---
+++ src/seatwork2/WhiteBoard.java
@@ -0,0 +1,9 @@
+package seatwork2;
+
+public class WhiteBoard {
+	int addNumbers(int addend1, int addend2) {
+		int addnum = addend1 + addend2;
+		return addnum;
+	}
+
+}
---
+++ src/seatwork2/seatwork2.java
@@ -0,0 +1,10 @@
+package seatwork2;
+
+public class seatwork2 {
+	public static void main(String[] args) {
+		
+		WhiteBoard wb = new WhiteBoard();
+		int sum2 = wb.addNumbers(5, 3);
+		System.out.println("The total is: " + String.valueOf(sum2));
+	}
+}
