3290d042e67412f0c43771fa41251e4827a39fff
Raymond Guiller N. Lam Ko <rgnlamko@headoffice.corp.bpi.com.ph>
2025-12-11 16:39:58 +0800

    seatwork

---
+++ m2_activity5/src/m2_activity5/MSWord.java
@@ -0,0 +1,21 @@
+package m2_activity5;
+
+public class MSWord extends Program {
+	
+	public MSWord(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");
+		this.setIsRunning(true);
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+        this.setIsRunning(false);
+	}
+}
+
